d623f5c58dd105edc483934df2b2c5d89b3d7931
Fiach Reid <fiach.reid@gmail.com>
2018-04-27 17:12:33 +0100

    Initial Commit

---
+++ RemoteLogCat/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RemoteLogCat
+//
+//  Created by user909680 on 4/27/18.
+//  Copyright © 2018 user909680. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
